5e1984e7095bb35e773bdae966b9ef5704f6ff6e
Jaremy Creechley <creechley@gmail.com>
2016-12-15 14:33:16 -0700

    Adding scripts to get changelog messages from main site.

--- update-radio-head-version.sh
+++ update-radio-head-version.sh
@@ -12,3 +12,10 @@ unzip -o RadioHead-${RH_VER}.zip
 
 rm RadioHead-${RH_VER}.zip
 
+VER_MSG=`html2text http://www.airspayce.com/mikem/arduino/RadioHead/ | ruby -ne 'puts $_ if /\s+ (\d+.\d+) \s+ (\d{4}-\d{2}-\d+) (.+)$/x' | grep ${RH_VER}` 
+
+echo $VER_MSG >> changelog.txt
+
+echo "Radio Head version release message: "
+echo $VER_MSG
+
