d70d70e3a0882c9c31b109fd0eb45dfc2e2f50ae
Philippe Rochat <fake236@hispeed.ch>
2017-07-06 19:20:36 +0200

    Added 2 examples
    With LoRa and encryption

---
+++ RadioHead/examples/rf95/rf95_encrypted_client/rf95_encrypted_client.ino
@@ -0,0 +1,41 @@
+/* Loca Simple Hello World Client with encrypted communications 
+ *  Philippe.Rochat'at'gmail.com
+ *  06.07.2017
+ */
+#include <RH_RF95.h>
+#include <RHEncryptedDriver.h>
+#include <XXTEA.h>
+#include <String.h>
+
+RH_RF95 rf95;     // Instanciate a LoRa driver
+XXTEA myCipher;   // Instanciate an XXTEA block ciphering
+RHEncryptedDriver myDriver(rf95, myCipher); // Instantiate the driver with those two
+
+float frequency = 868.0; // Change the frequency here. 
+unsigned char encryptkey[16]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}; // The very secret key
+char HWMessage[] = "Hello World !";
+uint8_t HWMessageLen;
+
+void setup() {
+  HWMessageLen = strlen(HWMessage);
+  Serial.begin(9600);
+  while (!Serial) ; // Wait for serial port to be available
+  Serial.println("LoRa Simple_Encrypted Client");
+  if (!rf95.init())
+    Serial.println("LoRa init failed");
+  // Setup ISM frequency
+  rf95.setFrequency(frequency);
+  // Setup Power,dBm
+  rf95.setTxPower(13);
+  myCipher.setKey(encryptkey, 16);
+  delay(4000);
+  Serial.println("Setup completed");
+}
+
+void loop() {
+  uint8_t data[myCipher.blockSize()] = {0};
+  for(uint8_t i = 0; i< HWMessageLen; i++) data[i] = (uint8_t)HWMessage[i];
+  Serial.print("Sending: "); Serial.println((char *)&data);
+  myDriver.send(data, sizeof(data)); // Send out ID + Sensor data to LoRa gateway
+  delay(4000);
+}
